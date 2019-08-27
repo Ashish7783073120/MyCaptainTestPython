86d1574995988225d8f34ed80af3924cc4e7aaff
Ashish7783073120 <54587480+Ashish7783073120@users.noreply.github.com>
2019-08-27 20:39:32 +0530

    Update Task-9.py

--- Task-9.py
+++ Task-9.py
@@ -1,1 +1,11 @@
-
+# Python-beginners-tasks
+numbers  = (1,2,3,4,5,6,7,8,9)
+odd_numbers = 0
+even_numbers = 0
+for x in numbers:
+    if x % 2==0:
+            even_numbers+=1
+    else:
+            odd_numbers+=1
+print("Number of even numbers :",even_numbers)
+print("Number of odd numbers :",odd_numbers)
