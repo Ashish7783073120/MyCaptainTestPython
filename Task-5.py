d94cbc471e399159b544dc260d28e78ece95d514
Ashish7783073120 <54587480+Ashish7783073120@users.noreply.github.com>
2019-08-27 20:35:09 +0530

    Update Task-5.py

--- Task-5.py
+++ Task-5.py
@@ -1,1 +1,5 @@
-
+# Python-beginners-tasks
+months = {"January":31 ,"February":28 ,"March":31 ,"April":30 ,"May":31 ,"June":30 ,"July":31 ,"August":31 ,"September":30 ,"October":31 ,"November":30 ,"December":31 }
+a = str(input("Enput the month name to get the no. of days in the month : "))
+a = a.captitalize()
+print(months.get(a))
