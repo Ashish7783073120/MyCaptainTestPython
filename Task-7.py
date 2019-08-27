7f18b021dcb66c243e70259e40de3409663ee6c1
Ashish7783073120 <54587480+Ashish7783073120@users.noreply.github.com>
2019-08-27 20:37:19 +0530

    Update Task-7.py

--- Task-7.py
+++ Task-7.py
@@ -1,1 +1,7 @@
-
+# Python-beginners-tasks
+from statistics import median
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+c = int(input("Enter a number"))
+d = [a,b,c]
+print(median(d))
