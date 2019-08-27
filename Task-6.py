6519d4a8517e065d35f601ede3c0e4e3393dd0b3
Ashish7783073120 <54587480+Ashish7783073120@users.noreply.github.com>
2019-08-27 20:36:37 +0530

    Update Task-6.py

--- Task-6.py
+++ Task-6.py
@@ -1,1 +1,10 @@
-
+# Python-beginners-tasks
+ab= int(input("Enter the size of a side of the triangle : "))
+bc= int(input("Enter the size of a side of the triangle : "))
+ca= int(input("Enter the size of a side of the triangle : "))
+if ab==bc==ca :
+   print("Equilateral Triangle")
+elif ab==bc or bc==ca or ca==ab :
+   print("Isosceles Triangle")
+else :
+   print("Scalene Triangle")
