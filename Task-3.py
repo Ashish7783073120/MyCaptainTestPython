73aa9038e8fdb2b76df78daa549a27d21821b580
Ashish7783073120 <54587480+Ashish7783073120@users.noreply.github.com>
2019-08-27 20:32:45 +0530

    Update Task-3.py

--- Task-3.py
+++ Task-3.py
@@ -1,1 +1,8 @@
-
+# Python-beginners-tasks
+def longest_word(words):
+    word_len = []
+    for x in words:
+        word_len.append((len(x), x))
+    word_len.sort()
+    return word_len[-1][1]
+print(longest_word(["Ashish", "Kumar", "Bhuwaniya"]))
